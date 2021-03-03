cc34b168a46355f72e8dfa6c1cd71fc7d1e6feb1
yulia-gaisina <79998300+yulia-gaisina@users.noreply.github.com>
2021-03-03 23:10:50 +0500

    Create Recharts.sh

---
+++ Recharts.sh
@@ -0,0 +1,7 @@
+<LineChart width={500} height={300} data={data}>
+<XAxis dataKey="name" />
+<YAxis />
+<CartesianGrid stroke="#eee" strokeDasharray="5 5" />
+<Line type="monotone" dataKey="uv" stroke="#8884d8" />
+<Line type="monotone" dataKey="pv" stroke="#82ca9d" />
+</LineChart>
